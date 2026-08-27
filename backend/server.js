a694d8ebf5a3f7f679beb992efdde144b6744ded
pradeep28dev <pradeep28.dev@gmail.com>
2026-08-27 10:53:22 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -67,6 +67,6 @@ app.get("/", (req, res) => {
 // ===============================
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
 
